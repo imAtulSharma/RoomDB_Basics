8c2b25323a22ce46bbf637dbe147d2f0bca0936b
Atul Sharma <atulsharma20may@gmail.com>
2023-05-15 15:40:35 +0530

    Initial commit

---
+++ app/src/main/java/android/example/roomdb/Word.java
@@ -0,0 +1,23 @@
+package android.example.roomdb;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "Word")
+    private String mWord;
+
+    public Word(@NonNull String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
+}
